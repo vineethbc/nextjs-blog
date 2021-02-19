a4c865b8afc7c4555691c2aaaaa5420766380935
Vineeth <vineethbc@gmail.com>
2021-02-20 00:14:53 +0530

    Update nextjs config for deployment

--- components/layout.js
+++ components/layout.js
@@ -6,6 +6,7 @@ import Link from 'next/link'
 
 const name = 'Vineeth'
 export const siteTitle = 'Next.js Sample Website'
+const deploymentUrl = '/nextjs-blog'
 
 export default function Layout({ children, home }) {
     return (
@@ -30,7 +31,7 @@ export default function Layout({ children, home }) {
                     <>
                         <img
                             priority
-                            src="/images/profile.jpg"
+                            src={deploymentUrl + "/images/profile.jpg"}
                             className={utilStyles.borderCircle}
                             height={144}
                             width={144}
@@ -44,7 +45,7 @@ export default function Layout({ children, home }) {
                                 <a>
                                     <img
                                         priority
-                                        src="/images/profile.jpg"
+                                        src={deploymentUrl + "/images/profile.jpg"}
                                         className={utilStyles.borderCircle}
                                         height={108}
                                         width={108}
